68bbb8d60fa0e84d008691c96c7f775e6b9a9a84
JoseRazo <joserrp.13@gmail.com>
2025-08-05 11:11:02 -0600

    change image about us

--- resources/views/front/components/about-component.blade.php
+++ resources/views/front/components/about-component.blade.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="row">
             <div class="col-md-6 d-flex align-items-stretch">
-                <div class="about-wrap img w-100" style="background-image: url(assets/images/about2.JPG);">
+                <div class="about-wrap img w-100" style="background-image: url({{ asset('storage/' . $siteSettings->about_us_image) }});">
                     <div class="icon d-flex align-items-center justify-content-center"><span
                             class="flaticon-crane"></span></div>
                 </div>
